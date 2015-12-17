af21996316fe1b6aadfec5d8a619e7831bb3f413
Danny Ruiz <ynnadrules@gmail.com>
2015-12-17 06:12:23 -0600

    Refactor to make easier to test

--- lib/simplecov/notifications.rb
+++ lib/simplecov/notifications.rb
@@ -1,3 +1,4 @@
+require 'simplecov'
 require "simplecov/notifications/version"
 require 'octokit'
 
@@ -12,97 +13,100 @@ module SimpleCov
   module Formatter
     class Notifications
 
-      attr_reader :last_report
+      class CircleCiBuild
+        attr_reader :owner,
+          :username, :repo,
+          :build, :pull_request,
+          :sha1, :artifacts_path
 
-      def last_report
-        @last_report ||= SimpleCov::LastRun.read || {}
+        def initialize(env)
+          @owner = env['CIRCLE_PROJECT_USERNAME']
+          @username = env['CIRCLE_USERNAME']
+          @repo = env['CIRCLE_PROJECT_REPONAME']
+          @build = env['CIRCLE_BUILD_NUM']
+          @pull_request = env['CI_PULL_REQUEST']
+          @sha1 = env['CIRCLE_SHA1']
+          @artifacts_path = env['CIRCLE_ARTIFACTS']
+        end
+
+        def repo_path
+          "#{owner}/#{repo}"
+        end
+
+        def artifacts_url
+          "https://circleci.com/gh/#{repo_path}/#{build}/#{artifacts_path}/"
+        end
+
+        def pull_request
+          @pull_request[/([\d]+)\/?$/]
+        end
+
+      end
+
+      attr_reader :last_run, :ci, :current_coverage,
+       :coverage_drop, :last_coverage
+      attr_accessor :coverage_data
+
+      def initialize(env = ENV)
+        @ci = CircleCiBuild.new(env)
       end
 
       def format(result)
-        current_coverage = result.covered_percent
-        return unless last_report['result']
+        last_run = SimpleCov::LastRun.read
+        return unless last_run
 
-        previous_coverage = last_report['result']['covered_percent']
+        self.current_coverage = result.covered_percent
+        self.last_coverage = last_run['result']['covered_percent']
 
-        coverage_data = {
-          current_coverage: current_coverage,
-          previous_coverage: previous_coverage
-        }
 
-        if previous_coverage > current_coverage
-          coverage_drop = previous_coverage - current_coverage
-          coverage_data[:coverage_drop] = coverage_drop
+        if last_coverage > current_coverage
+          self.coverage_drop = last_coverage - current_coverage
         end
 
-        send_status coverage_data
+        github.create_status(ci.repo_path, ci.sha1, status,
+          context: 'simplecov/notifications',
+          target_url: ci.artifacts_url,
+          description: status_description
+        )
+
+        if ci.pull_request && coverage_drop
+          github.add_comment(
+            ci.repo_path,
+            ci.pull_request,
+            comment_description
+          )
+        end
       end
 
       private
 
+      attr_writer :current_coverage, :coverage_drop, :last_coverage
       attr_reader :github
 
       def github
         @github ||= Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
       end
 
-      def send_status(data)
-        puts "Sending: #{data}"
-        puts "ENV: #{ENV}"
-
-        unless ENV['CIRCLECI']
-          puts "Data: #{data}"
-          return
-        end
-
-        # github.create_status(
-        #   owner_repo_name,
-        #   ENV['CIRCLE_SHA1'],
-        #   coverage_check_status(data),
-        #   context: 'simplecov/notifications',
-        #   target_url: coverage_report_url,
-        #   description: coverage_report_short_description(data)
-        # )
-
-        if ENV['CI_PULL_REQUEST']
-          github.add_comment(
-            owner_repo_name,
-            pull_request_number,
-            coverage_report_description(data)
-          )
-        end
-      end
-
-      def coverage_check_status(data)
-        if data[:coverage_drop]
+      def status
+        if coverage_drop
           'failure'
         else
           'success'
         end
       end
 
-      def coverage_report_url
-        "https://circleci.com/gh/#{ENV['CIRCLE_PROJECT_USERNAME']}/#{ENV['CIRCLE_PROJECT_REPONAME']}/#{ENV['CIRCLE_BUILD_NUM']}/#{ENV['CIRCLE_ARTIFACTS']}/index.html"
-      end
-
-      def coverage_report_short_description(data)
-        if data[:coverage_drop]
-          "Code coverage dropped by *#{data[:coverage_drop]}%*."
+      def status_description
+        if coverage_drop
+          "Code coverage dropped by #{coverage_drop}%."
+        else
+          "Current code coverage is at #{current_coverage}%"
         end
       end
 
-      def coverage_report_description(data)
-        if data[:coverage_drop]
-          "@#{ENV['CIRCLE_USERNAME']}: Your last push resulted in a *#{data[:coverage_drop]}%* code coverage drop from *#{data[:previous_coverage]}%* to *#{data[:current_coverage]}*."
-        end
+      def comment_description
+        "@#{ci.username}: Your last push resulted in a *#{coverage_drop}%* code coverage drop from *#{last_coverage}%* to *#{current_coverage}%*."
       end
 
-      def owner_repo_name
-        "#{ENV['CIRCLE_PROJECT_USERNAME']}/#{ENV['CIRCLE_PROJECT_REPONAME']}"
-      end
-
-      def pull_request_number
-        ENV['CI_PULL_REQUEST'][/([\d]+)\/?$/]
-      end
     end
   end
 end
--- spec/simplecov/notifications_spec.rb
+++ spec/simplecov/notifications_spec.rb
@@ -1,11 +1,130 @@
 require 'spec_helper'
+require 'pry'
 
-describe SimpleCov::Notifications do
-  it 'has a version number' do
-    expect(SimpleCov::Notifications::VERSION).not_to be nil
+describe SimpleCov::Formatter::Notifications do
+
+  let(:env) do
+    {
+      'CIRCLECI' => nil,
+      'CI_PULL_REQUEST' => '88',
+      'CIRCLE_SHA1' => '9fc29099caa165efef809d67fc274abba5c8c8fa',
+      'CIRCLE_PROJECT_USERNAME' => 'tastyworks',
+      'CIRCLE_PROJECT_REPONAME' => 'order-api',
+      'CIRCLE_BUILD_NUM' => '458',
+      'CIRCLE_ARTIFACTS' => '0/some-random-path/to/artifacts',
+    }
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  subject { described_class.new env }
+
+  it 'has a version number' do
+    expect(SimpleCov::Formatter::Notifications::VERSION).not_to be nil
+  end
+
+  describe 'circle ci build' do
+    let(:ci) { described_class::CircleCiBuild.new env }
+
+    it 'has a valid repo path' do
+      expect(ci.repo_path).to eq "#{env['CIRCLE_PROJECT_USERNAME']}/#{env['CIRCLE_PROJECT_REPONAME']}"
+    end
+
+    it 'has a valid artifacts url' do
+      expect(ci.artifacts_url).to include env['CIRCLE_ARTIFACTS']
+    end
+
+    it 'has a valid pull request ID' do
+      expect(ci.pull_request).to eq env['CI_PULL_REQUEST']
+    end
+  end
+
+  describe 'github integration' do
+    let(:ci) { described_class::CircleCiBuild.new env  }
+    let(:github) { instance_double('Octokit::Client') }
+    let(:high_coverage_result) { double('SimpleCov::Result', covered_percent: 98) }
+    let(:low_coverage_result) { double('SimpleCov::Result', covered_percent: 50) }
+    let(:low_coverage_run) do
+      { 'result' => { 'covered_percent' => 23 } }
+    end
+
+    let(:high_coverage_run) do
+      { 'result' => { 'covered_percent' => 100} }
+    end
+
+    before do
+      allow(subject).to receive(:ci).and_return(ci)
+      allow(subject).to receive(:github).and_return(github)
+    end
+
+    context 'no previous coverage data exists' do
+      it 'will not create a status' do
+        expect(github).not_to receive(:create_status)
+        subject.format(high_coverage_result)
+      end
+
+      it 'will not create a PR comment' do
+        expect(github).not_to receive(:add_comment)
+        subject.format(high_coverage_result)
+      end
+    end
+
+    context 'previous coverage data exists' do
+      context 'without a PR' do
+        before(:each) do
+          allow(SimpleCov::LastRun).to receive(:read).and_return(low_coverage_run)
+          allow(ci).to receive(:pull_request).and_return(nil)
+        end
+
+        it 'will create a status and no comment' do
+          expect(github).to receive(:create_status).with(
+            ci.repo_path,
+            ci.sha1,
+            'success',
+            context: 'simplecov/notifications',
+            target_url: ci.artifacts_url,
+            description: "Current code coverage is at #{high_coverage_result.covered_percent}%"
+          )
+
+          expect(github).not_to receive(:add_comment)
+
+          subject.format(high_coverage_result)
+        end
+      end
+
+      context 'with a PR' do
+        it 'will create a status' do
+          allow(SimpleCov::LastRun).to receive(:read).and_return(low_coverage_run)
+          expect(github).to receive(:create_status).with(
+            ci.repo_path,
+            ci.sha1,
+            'success',
+            context: 'simplecov/notifications',
+            target_url: ci.artifacts_url,
+            description: "Current code coverage is at #{high_coverage_result.covered_percent}%"
+          )
+
+          subject.format(high_coverage_result)
+        end
+
+        it 'will create a comment if there is a coverage drop' do
+          allow(SimpleCov::LastRun).to receive(:read).and_return(high_coverage_run)
+          expect(github).to receive(:create_status)
+          expect(github).to receive(:add_comment).with(
+            ci.repo_path,
+            ci.pull_request,
+            /#{subject.coverage_drop}%/
+          )
+
+          subject.format(low_coverage_result)
+        end
+
+        it 'will not create a comment if there is no coverage drop' do
+          allow(SimpleCov::LastRun).to receive(:read).and_return(low_coverage_run)
+          expect(github).to receive(:create_status)
+          expect(github).not_to receive(:add_comment)
+
+          subject.format(high_coverage_result)
+        end
+      end
+    end
   end
 end
